85c1b36f144df360a2a3983f448ef0afb7eb6750
Hazem Krimi <krimihazem1@gmail.com>
2020-10-26 23:42:01 +0100

    Add search screen

---
+++ src/screens/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SearchProps } from '../types/types';
+
+const Search: React.FC<SearchProps> = ({ route, navigation }) => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Search;
